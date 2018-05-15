d28948bdb360fe7a25ecdf3919c66cafebdb382a
Gundars Pelns <Gundars2.Pelns@live.uwe.ac.uk>
2018-05-15 16:12:04 +0100

    Mouse invisible and stays in middle
    to show mouse press Esc.. click on screen to hide mouse

--- BuilderWorld/Assets/Scripts/CameraMovement.cs
+++ BuilderWorld/Assets/Scripts/CameraMovement.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float mainSpeed = 100.0f; //regular speed
-    public float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
-    public float maxShift = 1000.0f; //Maximum speed when holdin gshift
-    public float camSens = 0.25f; //How sensitive it with mouse
+    public float flySpeed = 10.0f; //regular speed
+    public float shiftFly = 20.0f; //multiplied by how long shift is held.  Basically running
+    public float maxShift = 25.0f; //Maximum speed when holdin gshift
+    public float camSens = 1.5f; //How sensitive it with mouse
 
     private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
     private float totalRun = 1.0f;
 
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     void Update()
     {
+        //When clicked lock again the mouse
+        if (Input.GetMouseButtonDown(0) )
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
         lastMouse = Input.mousePosition - lastMouse;
-        lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
+        lastMouse = new Vector3(-Input.GetAxis("Mouse Y") * camSens, Input.GetAxis("Mouse X") * camSens, 0);
         lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
         transform.eulerAngles = lastMouse;
         lastMouse = Input.mousePosition;
@@ -27,7 +38,7 @@ public class CameraMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftShift))
         {
             totalRun += Time.deltaTime;
-            p = p * totalRun * shiftAdd;
+            p = p * totalRun * shiftFly;
             p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
             p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
             p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
@@ -35,14 +46,15 @@ public class CameraMovement : MonoBehaviour
         else
         {
             totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-            p = p * mainSpeed;
+            p = p * flySpeed;
         }
 
         p = p * Time.deltaTime;
         Vector3 newPos = transform.position;
 
         if (Input.GetKey(KeyCode.Space))
-        { //If player wants to move on X and Z axis only
+        { 
+            //If player wants to move on X and Z axis only
             transform.Translate(p);
             newPos.x = transform.position.x;
             newPos.z = transform.position.z;
@@ -52,7 +64,6 @@ public class CameraMovement : MonoBehaviour
         {
             transform.Translate(p);
         }
-
     }
 
     private Vector3 GetBaseInput()
@@ -80,6 +91,7 @@ public class CameraMovement : MonoBehaviour
 
         return p_Velocity;
     }
+
 }
 
 //    {
